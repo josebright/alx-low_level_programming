5e4273dbf847f196568f15f903d6435839ab46f8
josebright <josebright29@gmail.com>
2022-03-10 11:33:26 -0800

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+
+/**
+ * A C program that prints exactly "Programming is like building a multilingual puzzle"
+ * Return: 0 if exited properly, non-zero otherwise
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
